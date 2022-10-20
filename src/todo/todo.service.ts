bf06ccaf40f43f226a92008828cf0e70c798f586
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-20 11:31:25 -0300

    feat: Create, Find and Update TODO added

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [
+    { id: 1, description: 'Piedra del Alma', done: false },
+    { id: 2, description: 'Piedra del Tiempo', done: false },
+    { id: 3, description: 'Piedra del Espacio', done: true },
+  ];
+
+  create({ description }: CreateTodoDto): Todo {
+    const todo = new Todo();
+    todo.id = Math.max(...this.todos.map((todo) => todo.id), 0) + 1;
+    todo.description = description;
+    this.todos.push(todo);
+    return todo;
+  }
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOne(id: number): Todo {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (!todo) throw new NotFoundException(`TODO with id: ${id} not found`);
+    return todo;
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
